4fd24a0b1dfd90ee550b4163e10f826bb01a25ea
lislmaka <70829116+lislmaka@users.noreply.github.com>
2021-01-28 02:28:26 +0300

    add 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,14 @@
-@extends('errors::minimal')
+@extends('layouts.app')
 
 @section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+@section('content')
+    <div class="text-center my-5">&nbsp;</div>
+    <div class="h2 text-center fw-bold">&#128533; Упс... Нет такой страницы</div>
+    <div class="lead text-dark text-center pt-3 px-5">
+        Запрашиваемой страницы
+        <br><span class="fw-bold">{{ Request::url() }}</span><br>
+        не найдено на нашем сайте
+    </div>
+    <div class="text-center my-5">&nbsp;</div>
+@endsection
+
