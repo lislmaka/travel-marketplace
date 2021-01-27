28ec891554e8f654589ef90a8d4937d2d83e8cd1
lislmaka <70829116+lislmaka@users.noreply.github.com>
2021-01-28 02:51:50 +0300

    fix 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,13 +2,26 @@
 
 @section('title', __('Not Found'))
 @section('content')
-    <div class="text-center my-5">&nbsp;</div>
-    <div class="h2 text-center fw-bold">&#128533; Упс... Нет такой страницы</div>
-    <div class="lead text-dark text-center pt-3 px-5">
-        Запрашиваемой страницы
-        <br><span class="fw-bold">{{ Request::url() }}</span><br>
-        не найдено на нашем сайте
+    <div class="my-3">&nbsp;</div>
+    <div class="row d-flex justify-content-center">
+{{--        <div class="col-md-4 vh-100 d-flex align-items-center ">--}}
+        <div class="col-md-6 my-5 d-flex align-items-center">
+            <div class="d-flex flex-column w-100">
+                <div class="card bg-transparent border-0">
+                    <div class="card-body text-center lead">
+                        <div class="card-title fw-bold h2 text-center my-3">
+                            &#128533; @lang('Упс... Нет такой страницы')
+                        </div>
+
+                        @lang('Запрашиваемой страницы')
+                        <div class="fw-bold">{{ Request::url() }}</div>
+                        @lang('не найдено на нашем сайте')
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
-    <div class="text-center my-5">&nbsp;</div>
+    <div class="my-3">&nbsp;</div>
 @endsection
 
