4b0f21f5d8209f170add252be28eebad5640abc0
lislmaka <70829116+lislmaka@users.noreply.github.com>
2021-01-30 11:19:37 +0300

    add new block to event view

---
+++ resources/views/site/frontend/event/desktop/blocks/photos_header.blade.php
@@ -0,0 +1,9 @@
+<div class="row row-cols-md-4 g-0 mt-0">
+    @php(shuffle($demo_images))
+    @foreach($demo_images as $demo_image)
+        @if($loop->index > 7)
+            @break
+        @endif
+    <div class="col"><img src="{{ asset('images/demo/demo1/'.$demo_image->getBaseName()) }}" class="img-fluid" alt="..."></div>
+    @endforeach
+</div>
--- resources/views/site/frontend/event/layout.blade.php
+++ resources/views/site/frontend/event/layout.blade.php
@@ -6,51 +6,54 @@
 @endsection
 
 @section('content')
-    {{-- Begin Desktop --}}
-    <div class="container-xxl">
-        <div class="d-none d-md-block">
-            @includeIf('site.def.breadcrumb')
-            <div class="row">
-                <div class="col-md-9">
-                    @includeIf('site.frontend.event.desktop.blocks.event_info')
-                    @includeIf('site.frontend.event.desktop.blocks.order')
-                    @includeIf('site.frontend.event.desktop.blocks.photos')
-                    @includeIf('site.frontend.event.desktop.blocks.city')
-                    @includeIf('site.frontend.event.desktop.blocks.similars')
-                    @includeIf('site.frontend.event.desktop.blocks.reviews')
-                </div>
-                <div class="col-md-3">
-                    <div class="sticky-top sticky-offset">
-                        @includeIf('site.frontend.event.desktop.blocks.price')
+    @if ($agent->isDesktop())
+        {{-- Begin Desktop --}}
+        <div class="container-xxl">
+            <div class="d-none d-md-block">
+                <div class="row">
+                    <div class="col-md-12">
+                        @includeIf('site.frontend.event.desktop.blocks.photos_header')
                     </div>
                 </div>
-            </div>
+                @includeIf('site.def.breadcrumb')
+                <div class="row">
+                    <div class="col-md-9">
+                        @includeIf('site.frontend.event.desktop.blocks.event_info')
+                        @includeIf('site.frontend.event.desktop.blocks.order')
+                        @includeIf('site.frontend.event.desktop.blocks.photos')
+                        @includeIf('site.frontend.event.desktop.blocks.city')
+                        @includeIf('site.frontend.event.desktop.blocks.similars')
+                        @includeIf('site.frontend.event.desktop.blocks.reviews')
+                    </div>
+                    <div class="col-md-3">
+                        <div class="sticky-top sticky-offset">
+                            @includeIf('site.frontend.event.desktop.blocks.price')
+                        </div>
+                    </div>
+                </div>
 
-{{--            <div class="row">--}}
-{{--                <div class="col-md-12">--}}
-{{--                    @includeIf('site.frontend.event.desktop.blocks.city')--}}
-{{--                    @includeIf('site.frontend.event.desktop.blocks.similars')--}}
-{{--                    @includeIf('site.frontend.event.desktop.blocks.reviews')--}}
-{{--                </div>--}}
-{{--            </div>--}}
-        </div>
-    </div>
-    {{-- End Desktop --}}
-
-    {{-- Begin Mobile --}}
-    <div class="d-block d-md-none">
-        @includeIf('site.def.breadcrumb')
-
-        <div class="row">
-            <div class="col-md-3">
-
-            </div>
-            <div class="col-md-9">
 
             </div>
         </div>
-    </div>
-    {{-- End Mobile --}}
+        {{-- End Desktop --}}
+    @endif
+
+    @if ($agent->isMobile())
+        {{-- Begin Mobile --}}
+        <div class="d-block d-md-none">
+            @includeIf('site.def.breadcrumb')
+
+            <div class="row">
+                <div class="col-md-3">
+
+                </div>
+                <div class="col-md-9">
+
+                </div>
+            </div>
+        </div>
+        {{-- End Mobile --}}
+    @endif
 @endsection
 
 @push('scripts')
