170e18980406600cbfba29d783ece97c17bad17d
lislmaka <70829116+lislmaka@users.noreply.github.com>
2021-02-02 11:22:54 +0300

    refactoring search to events

--- resources/views/site/frontend/events/desktop/blocks/search-horizontal.blade.php
+++ resources/views/site/frontend/events/desktop/blocks/search-horizontal.blade.php
@@ -3,18 +3,16 @@
         <div class="row d-flex justify-content-center">
             <div class="col-md-8 d-flex align-items-center position-relative p-0">
                 <div class="input-group input-group-lg">
-                    <input type="text" class="form-control" placeholder="@lang('Введите страну, город или название тура')"
-                           aria-label="Recipient's username" aria-describedby="button-addon2">
-                    <button class="btn btn-primary btn-lg" type="button" id="button-addon2">
-                        @lang('Поиск туров')
-                    </button>
+                    <span class="input-group-text" id="inputGroup-sizing-lg"><i class="fas fa-map-marker-alt"></i></span>
+                    <input type="text" class="form-control" placeholder="@lang('Выберите страну или город')">
                 </div>
                 <div class="position-absolute top-0 start-0 mt-5 w-100" style="z-index: 10">
                     <div class="card border-light mt-1 w-100">
-                        <div class="card-header">
-                            Выберите интересующую вас страну и город
-                        </div>
                         <ul class="list-group list-group-flush">
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                <span class="fw-bold">@lang('Место')</span>
+                                <span class="fw-bold">@lang('Кол-во туров')</span>
+                            </li>
                             <li class="list-group-item d-flex justify-content-between align-items-center">
                                 Cras justo odio
                                 <span class="badge bg-primary rounded-pill">14</span>
