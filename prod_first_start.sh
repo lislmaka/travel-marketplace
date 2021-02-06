f30c6ee23bce0c21fe11ae41cbf37ee81698c6df
lislmaka <70829116+lislmaka@users.noreply.github.com>
2021-02-06 11:30:51 +0300

    fix prod sh script

--- prod_first_start.sh
+++ prod_first_start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 /opt/php/7.3/bin/php composer.phar install --no-dev --optimize-autoloader
-cp .env.example .env
 /opt/php/7.3/bin/php artisan key:generate
 /opt/php/7.3/bin/php artisan migrate:fresh --seed
 /opt/php/7.3/bin/php artisan config:cache
