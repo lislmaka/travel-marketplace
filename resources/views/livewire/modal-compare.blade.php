f9103a4cb633fb580a3a86e042c08551ee5f2112
lislmaka <70829116+lislmaka@users.noreply.github.com>
2021-02-14 20:19:42 +0300

    add loading spinner to compare page

--- resources/views/livewire/modal-compare.blade.php
+++ resources/views/livewire/modal-compare.blade.php
@@ -1,3 +1,26 @@
-<div>
-    @include('site.components.desktop.modal-events-compare',['events' => $events])
+{{--<div>--}}
+{{--    @include('site.components.desktop.modal-events-compare',['events' => $events])--}}
+{{--</div>--}}
+
+<div class="modal-content">
+    <div class="modal-header bg-secondary text-white">
+        <div class="modal-title lead fw-bold d-flex justify-content-start align-items-center" id="eventsCompareLabel">
+            @lang('Сравнение туров')
+            <div wire:loading.flex>
+                <span class="spinner-grow spinner-grow-sm ms-1" role="status" aria-hidden="true"></span>
+            </div>
+        </div>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+        @include('site.components.desktop.modal-events-compare',['events' => $events])
+    </div>
+    <div class="modal-footer">
+        <a class="btn btn-primary" href="{{ route('events.events_compare_clean') }}" role="button">
+            @lang('Очистить')
+        </a>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+            @lang('Закрыть')
+        </button>
+    </div>
 </div>
--- resources/views/site/def/modals/modal_events_compare.blade.php
+++ resources/views/site/def/modals/modal_events_compare.blade.php
@@ -2,26 +2,8 @@
 <div class="modal fade" id="eventsCompare" tabindex="-1" aria-labelledby="eventsCompareLabel"
      aria-hidden="true">
     <div class="modal-dialog modal-xl modal-fullscreen-xxl-down modal-dialog-scrollable">
-        <div class="modal-content">
-            <div class="modal-header bg-secondary text-white">
-                <div class="modal-title lead fw-bold" id="eventsCompareLabel">
-                    @lang('Сравнение туров')
-                </div>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                {{-- Modal Compare Begin --}}
-                @livewire('modal-compare')
-                {{-- Modal Compare End --}}
-            </div>
-            <div class="modal-footer">
-                <a class="btn btn-primary" href="{{ route('events.events_compare_clean') }}" role="button">
-                    @lang('Очистить')
-                </a>
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-                    @lang('Закрыть')
-                </button>
-            </div>
-        </div>
+        {{-- Modal Compare Begin --}}
+        @livewire('modal-compare')
+        {{-- Modal Compare End --}}
     </div>
 </div>
