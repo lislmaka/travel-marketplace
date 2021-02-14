a07430e73733cd4f266dd65cb79fb956e4992f8f
lislmaka <70829116+lislmaka@users.noreply.github.com>
2021-02-14 20:27:50 +0300

    add fix to loading spinner / compare page

--- resources/views/livewire/modal-compare.blade.php
+++ resources/views/livewire/modal-compare.blade.php
@@ -7,7 +7,11 @@
         <div class="modal-title lead fw-bold d-flex justify-content-start align-items-center" id="eventsCompareLabel">
             @lang('Сравнение туров')
             <div wire:loading.flex>
-                <span class="spinner-grow spinner-grow-sm ms-1" role="status" aria-hidden="true"></span>
+                <span class="badge bg-light text-muted ms-3">
+                    <span class="spinner-grow spinner-grow-sm text-primary" role="status" aria-hidden="true"></span>
+                    @lang('Идет обновление...')
+                </span>
+
             </div>
         </div>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
