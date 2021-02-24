d948abd89e78e0855f8f1c342d36893760e31031
lislmaka <70829116+lislmaka@users.noreply.github.com>
2021-02-24 12:00:56 +0300

    fix

--- resources/views/site/frontend/event/desktop/event-info.blade.php
+++ resources/views/site/frontend/event/desktop/event-info.blade.php
@@ -1,17 +1,17 @@
 <div class="card border-light">
     <div class="card-body d-flex justify-content-between align-items-start" id="description">
-        <div class="w-100">
-            <div class="card-title">
-                @foreach($event_info->categories as $category)
-                    <span class="badge bg-secondary">{{ $category->category->name }}</span>
-                @endforeach
-            </div>
-            <div class="card-title">
-                <span class="badge bg-secondary"><i class="fas fa-map-marker-alt"></i></span>
-                <span class="badge bg-secondary">{{ $event_info->country->name }}</span>
-                <span class="badge bg-secondary">{{ $event_info->city->name }}</span>
-            </div>
-        </div>
+{{--        <div class="w-100">--}}
+{{--            <div class="card-title">--}}
+{{--                @foreach($event_info->categories as $category)--}}
+{{--                    <span class="badge bg-secondary">{{ $category->category->name }}</span>--}}
+{{--                @endforeach--}}
+{{--            </div>--}}
+{{--            <div class="card-title">--}}
+{{--                <span class="badge bg-secondary"><i class="fas fa-map-marker-alt"></i></span>--}}
+{{--                <span class="badge bg-secondary">{{ $event_info->country->name }}</span>--}}
+{{--                <span class="badge bg-secondary">{{ $event_info->city->name }}</span>--}}
+{{--            </div>--}}
+{{--        </div>--}}
         <div class="w-100 text-end">
 {{--            @livewire('btn-add-to-favorites')--}}
             @php
@@ -27,8 +27,47 @@
 
         </div>
     </div>
+    {{-- --}}
+    <ul class="list-group list-group-flush">
+
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span class="fw-bold">
+                @lang('Страна')
+            </span>
+            <span class="badge bg-secondary">{{ $event_info->country->name }}</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span class="fw-bold">
+                @lang('Город')
+            </span>
+            <span class="badge bg-secondary">{{ $event_info->city->name }}</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span class="fw-bold">
+                @lang('Продолжительность тура')
+            </span>
+            <span class="badge bg-secondary">
+                @php
+                    $interval = Carbon\CarbonInterval::make($event_info->duration.'h');
+                @endphp
+                {{ $interval->cascade()->forHumans() }}
+            </span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span class="fw-bold">
+                @lang('Категории тура')
+            </span>
+
+            <div>
+                @foreach($event_info->categories as $category)
+                    <span class="badge bg-secondary">{{ $category->category->name }}</span>
+                @endforeach
+            </div>
+        </li>
+    </ul>
+    {{-- --}}
     <div class="card-body">
-        <div class="card-text">
+        <div class="card-text mt-3">
             {!! $event_info->description !!}
         </div>
     </div>
